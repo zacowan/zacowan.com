12c4c0b1f973c6580efa76b5d477af4f9fc0ea97
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2024-07-08 21:25:51 -0400

    chore: update urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,12 @@
 import { getBlogPosts } from "app/blog/utils";
 import { z } from "zod";
 
+// https://vercel.com/docs/projects/environment-variables/system-environment-variables#system-environment-variables
 const vercelEnvironmentSchema = z.object({
+  VERCEL_ENV: z
+    .literal("production")
+    .or(z.literal("preview"))
+    .or(z.literal("development")),
   VERCEL_PROJECT_PRODUCTION_URL: z.string(),
   VERCEL_BRANCH_URL: z.string(),
   VERCEL_URL: z.string(),
@@ -12,10 +17,13 @@ const LOCAL_URL = "localhost:3000";
 const getBaseUrl = () => {
   try {
     const safeEnv = vercelEnvironmentSchema.parse(process.env);
-    console.log("Branch url", safeEnv.VERCEL_BRANCH_URL);
-    console.log("Vercel URL", safeEnv.VERCEL_URL);
-    console.log(`Using ${safeEnv.VERCEL_PROJECT_PRODUCTION_URL} as base url`);
-    return safeEnv.VERCEL_PROJECT_PRODUCTION_URL;
+    switch (safeEnv.VERCEL_ENV) {
+      case "development":
+      case "preview":
+        return `https://${safeEnv.VERCEL_URL}`;
+      default:
+        return `https://${safeEnv.VERCEL_PROJECT_PRODUCTION_URL}`;
+    }
   } catch (error) {
     console.log(
       `No vercel environment variables found, using ${LOCAL_URL} as base url:`,
