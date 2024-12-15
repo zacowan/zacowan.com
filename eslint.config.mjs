87a5961ddff896d45d56c8e083905688b55ec17c
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2024-12-15 12:49:33 -0500

    chore: eslint v9 (#52)
    * chore: eslint v9
    
    * chore: remove unnecessary tseslint deps, revert type: module

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: eslint.configs.recommended,
+  allConfig: eslint.configs.all,
+});
+
+export default tseslint.config(
+  ...compat.extends("next/core-web-vitals"),
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  },
+);
