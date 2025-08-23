7c70ae1ae4b0358d887f03e32e6ca9735b29432a
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2025-08-23 12:01:36 -0400

    set html color

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" className="bg-[#16213e]">
 			<body className={`${josefin.className}`}>
 				<AnimatedBackground>{children}</AnimatedBackground>
 			</body>
