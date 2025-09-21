84c37b7cbb8b8366ca300ae728d2c2c95a25bfbf
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2025-09-21 14:23:30 -0400

    lints

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,45 +10,45 @@ import TwitterIcon from "@/components/icons/twitter";
 const instrumentSerif = Instrument_Serif({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "zacOS",
-  description: "The operating system built for me.",
+	title: "zacOS",
+	description: "The operating system built for me.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className="bg-black text-white min-h-screen px-4 py-8 md:px-8 md:py-12"
-    >
-      <body className={`${instrumentSerif.className}`}>
-        <nav className="w-full flex justify-center items-center pb-12">
-          <Link href="/">Zach Cowan</Link>
-        </nav>
-        {children}
-        <footer className="w-full pt-24">
-          <ul className="flex items-center justify-center gap-6">
-            <li>
-              <a href="https://x.com/zacowan_">
-                <TwitterIcon />
-              </a>
-            </li>
-            <li>
-              <a href="https://linkedin.com/in/zacowan">
-                <LinkedInIcon />
-              </a>
-            </li>
-            <li>
-              <a href="https://instagram.com/zaocwan">
-                <InstagramIcon />
-              </a>
-            </li>
-          </ul>
-        </footer>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className="bg-black text-white min-h-screen px-4 py-8 md:px-8 md:py-12"
+		>
+			<body className={`${instrumentSerif.className}`}>
+				<nav className="w-full flex justify-center items-center pb-12">
+					<Link href="/">Zach Cowan</Link>
+				</nav>
+				{children}
+				<footer className="w-full pt-24">
+					<ul className="flex items-center justify-center gap-6">
+						<li>
+							<a href="https://x.com/zacowan_">
+								<TwitterIcon />
+							</a>
+						</li>
+						<li>
+							<a href="https://linkedin.com/in/zacowan">
+								<LinkedInIcon />
+							</a>
+						</li>
+						<li>
+							<a href="https://instagram.com/zaocwan">
+								<InstagramIcon />
+							</a>
+						</li>
+					</ul>
+				</footer>
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
 import Vercel from "@/components/icons/vercel";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center space-y-4">
-      <div className="text-4xl md:text-6xl">Engineering in NYC.</div>
-      <div className="text-3xl md:text-5xl text-neutral-400">
-        Exploring design & experience.
-      </div>
-      <div className="flex flex-col text-xl md:text-2xl gap-4 mt-12 items-center justify-center">
-        <div className="space-x-4">
-          <span>Building at</span>
-          <a
-            href="https://vercel.com/careers"
-            className="border-neutral-800 border rounded-full px-5 py-3 inline-flex items-center justify-center hover:bg-neutral-900"
-          >
-            <Vercel />
-          </a>
-        </div>
-        <div className="space-x-2 space-y-2 text-neutral-400 text-md md:text-lg text-center">
-          <span className="block md:inline">Previously at</span>
-          <span className="block md:inline">
-            <span className="border-neutral-800 border rounded-full px-3 py-1 inline-flex items-center justify-center">
-              American Express
-            </span>
-            <span className="border-neutral-800 border rounded-full px-3 py-1 inline-flex items-center justify-center">
-              Lockheed Martin
-            </span>
-          </span>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex flex-col items-center justify-center space-y-4">
+			<div className="text-4xl md:text-6xl">Engineering in NYC.</div>
+			<div className="text-3xl md:text-5xl text-neutral-400">
+				Exploring design & experience.
+			</div>
+			<div className="flex flex-col text-xl md:text-2xl gap-4 mt-12 items-center justify-center">
+				<div className="space-x-4">
+					<span>Building at</span>
+					<a
+						href="https://vercel.com/careers"
+						className="border-neutral-800 border rounded-full px-5 py-3 inline-flex items-center justify-center hover:bg-neutral-900"
+					>
+						<Vercel />
+					</a>
+				</div>
+				<div className="space-x-2 space-y-2 text-neutral-400 text-md md:text-lg text-center">
+					<span className="block md:inline">Previously at</span>
+					<span className="block md:inline">
+						<span className="border-neutral-800 border rounded-full px-3 py-1 inline-flex items-center justify-center">
+							American Express
+						</span>
+						<span className="border-neutral-800 border rounded-full px-3 py-1 inline-flex items-center justify-center">
+							Lockheed Martin
+						</span>
+					</span>
+				</div>
+			</div>
+		</main>
+	);
 }
