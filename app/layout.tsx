fa4a2498f55e2eb8a5c6759fae1337e5ece93cf7
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2025-09-21 14:03:25 -0400

    design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,52 @@
 import type { Metadata } from "next";
-import { Josefin_Sans } from "next/font/google";
+import { Instrument_Serif } from "next/font/google";
 import type React from "react";
 import "./globals.css";
 import AnimatedBackground from "@/components/animated-bg";
+import Link from "next/link";
+import LinkedInIcon from "@/components/icons/linkedin";
+import InstagramIcon from "@/components/icons/instagram";
+import TwitterIcon from "@/components/icons/twitter";
 
-const josefin = Josefin_Sans({ subsets: ["latin"] });
+const instrumentSerif = Instrument_Serif({ weight: "400" });
 
 export const metadata: Metadata = {
-	title: "zacOS",
-	description: "The operating system built for me.",
+  title: "zacOS",
+  description: "The operating system built for me.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" className="bg-[#16213e]">
-			<body className={`${josefin.className}`}>
-				<AnimatedBackground>{children}</AnimatedBackground>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className="bg-black text-white min-h-screen">
+      <nav className="w-full flex justify-center items-center px-8 py-12">
+        <Link href="/">Zach Cowan</Link>
+      </nav>
+      <body className={`${instrumentSerif.className} relative`}>
+        {children}
+      </body>
+      <footer className="w-full px-8 pt-24">
+        <ul className="flex items-center justify-center gap-6">
+          <li>
+            <a href="https://x.com/zacowan_">
+              <TwitterIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/in/zacowan">
+              <LinkedInIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://instagram.com/zaocwan">
+              <InstagramIcon />
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </html>
+  );
 }
--- rust/boxd/README.md
+++
@@ -1,3 +0,0 @@
-# Boxd
-
-A game about clicking the box as quickly as possible.
